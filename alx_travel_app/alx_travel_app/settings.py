c5faea5554354b769678f1685efe80ad2a17ecc7
alchemist1111 <emoyoeugine@gmail.com>
2025-09-07 21:46:21 +0300

    Make changes for the checker to pass

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -27,6 +27,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
